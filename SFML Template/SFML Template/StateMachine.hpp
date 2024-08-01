7521b5f8810b49676b266344e410b159d2ffceb3
janiceparkk <jparkowl@gmail.com>
2024-08-01 11:44:00 -0700

    Abstract main function into State & StateMachine for assest managment

---
+++ SFML Template/SFML Template/StateMachine.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+#include <stack>
+#include "State.hpp"
+
+namespace Sonar {
+    typedef std::unique_ptr<State> StateRef;
+
+    class StateMachine {
+        public:
+            StateMachine();
+            ~StateMachine(); // destructor
+            
+            void AddState(StateRef newState, bool isReplacing = true);
+            void RemoveState();
+            void ProcessStateChanges();
+            
+            StateRef &GetActiveState();
+        
+        private:
+            std::stack<StateRef> _states;
+            StateRef _newState;
+            bool _isAdding;
+            bool _isRemoving;
+            bool _isReplacing;
+    };
+}
+
--- SFML Template/SFML Template/main.cpp
+++ SFML Template/SFML Template/main.cpp
@@ -1,31 +1,5 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 1024
-
 int main() {
-    // Open Window
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Awesome Game");
-    
-    // Game Loop
-    while (window.isOpen()) {
-        // Handle inputs/events
-        sf::Event event;
-        
-        while (window.pollEvent(event)) {
-            switch (event.type) {
-                // Check if window is closed
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-            }
-        }
-        // Update Game Logic
-        window.clear();
-        
-        // Draw Objects
-        window.display();
-    }
     return EXIT_SUCCESS;
 }
